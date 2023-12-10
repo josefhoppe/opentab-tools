14fe5a62d0a6906881f193633954d152862afce1
Josef Hoppe <josef@hoppe.io>
2023-12-10 21:58:49 +0100

    add tabbycat feedback formatter

--- feedback_formatter.py
+++ feedback_formatter.py
@@ -2,6 +2,7 @@ from opentab_api import *
 from jinja2 import Environment, FileSystemLoader
 
 import random, os
+import xml.dom.minidom
 
 import numpy as np
 
@@ -17,6 +18,21 @@ def question_explanation(question: dict):
         return ''
     labels = params['labels']
     return ', '.join([ f'{k}: {v}' for k,v in labels.items()])
+
+def format_feedback_tc(judge: str, df_feedback: pd.DataFrame, jinja_env: Environment):
+    numerics = [ {
+        'title': 'Score',
+        'explanation': 'Numerische Gesamtwertung, gemittelt von Chairs, Wings, und Teams',
+        'mean': df_feedback['score'].mean()
+    } ]
+
+    others = [ {
+        'title': 'Feedback',
+        'contents': [ s for s in shuffled(df_feedback['answer'].to_list()) if s != '' ]
+    } ]
+
+    template = jinja_env.get_template("feedback.html")
+    return template.render(name=judge, numerics=numerics, others=others)
 
 def format_feedback(questions: List[dict], feedback: JudgeFeedback, jinja_env: Environment):
     numerics = [ {
@@ -42,4 +58,15 @@ def output_all_feedback(target_folder = 'feedback/'):
     for judge in feedback.feedback:
         res = format_feedback(feedback.questions, judge, environment)
         with open(f'{target_folder}{judge.token}.html', mode="w", encoding="utf-8") as fb_file:
+            fb_file.write(res)
+
+def output_all_feedback_tabbycat(target_folder = 'feedback/'):
+    environment = Environment(loader=FileSystemLoader("templates/"), autoescape=True)
+    df_feedback = pd.read_csv('feedback.csv')
+    df_judges = pd.read_csv('tablinks.csv', index_col=0)
+    print(df_judges)
+    os.makedirs(target_folder, exist_ok=True)
+    for judge in df_feedback.to.unique():
+        res = format_feedback_tc(judge, df_feedback[df_feedback.to == judge], environment)
+        with open(f'{target_folder}{df_judges.loc[judge]["url"]}.html', mode="w", encoding="utf-8") as fb_file:
             fb_file.write(res)
